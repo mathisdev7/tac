7e650af74bc3791c29f358c8e8642b0567aff64e
mathisdev7 <999frostz@gmail.com>
2025-02-12 18:46:06 +0100

    chore(debug): debugging env variables

--- tac-frontend/src/app/page.tsx
+++ tac-frontend/src/app/page.tsx
@@ -4,6 +4,7 @@ import useSWR from "swr";
 const fetcher = (url: string) => fetch(url).then((res) => res.text());
 
 export default function Home() {
+  console.log(process.env.NEXT_PUBLIC_TAC_API_URL);
   const { data } = useSWR(
     `${process.env.NEXT_PUBLIC_TAC_API_URL}/data?username=frost`,
     fetcher
